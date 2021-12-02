ff60b0c9222200b1a6ae1c4392f07cc6b87d60ba
nicco simone <niccosimone@niccos-MBP-2.domain>
2021-12-02 12:56:06 -0800

    wait, I'm a dummy
    looking back at day 1, all the numbers are positive,
    so I don't have to actually sum anything - just compare
    the window boundaries :facepalm:

--- aoc-2021/src/day_1.rs
+++ aoc-2021/src/day_1.rs
@@ -26,45 +26,14 @@ pub mod day_1 {
             .collect();
 
         lines
-            .windows(3)
-            .zip(lines[1..].windows(3))
-            .fold(0, |acc, (prev_window, curr_window)| {
-                let prev_sum: usize = prev_window.iter().sum();
-                let curr_sum: usize = curr_window.iter().sum();
-                if curr_sum > prev_sum {
+            .windows(4)
+            .fold(0, |acc, window| {
+                if window[3] > window[0] {
                     acc + 1
-                } else {
+                }
+                else {
                     acc
                 }
             })
-    }
-
-    // reduces redundant sums using memoization and sliding window
-    pub fn part_2_fast() -> usize {
-        let lines: Vec<usize> = utils::read_lines("input/day-1-1.txt")
-            .iter()
-            .map(|l| l.parse::<usize>().unwrap())
-            .collect();
-
-        lines
-            .windows(4)
-            .fold((0, 0), |acc, window| {
-                let prev_sum: usize;
-                let curr_sum: usize;
-                // initialize previous sum (only runs once)
-                if acc.0 == 0 {
-                    prev_sum = window[..=2].iter().sum();
-                } else {
-                    prev_sum = acc.0;
-                }
-                curr_sum = prev_sum - window[0] + window[3];
-
-                if curr_sum > prev_sum {
-                    (curr_sum, acc.1 + 1)
-                } else {
-                    (curr_sum, acc.1)
-                }
-            })
-            .1
     }
 }
--- aoc-2021/src/main.rs
+++ aoc-2021/src/main.rs
@@ -3,5 +3,5 @@ mod day_2;
 mod utils;
 
 fn main() {
-    print!("answer: {}", day_2::day_2::part_2());
+    print!("answer: {}", day_1::day_1::part_2_fast());
 }
