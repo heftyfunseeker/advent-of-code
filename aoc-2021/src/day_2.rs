66f3cbf676bc20edde3e97dac120ace81c081afa
nicco simone <niccosimone@niccos-MBP-2.domain>
2021-12-02 12:39:38 -0800

    Day 2 part 2

--- aoc-2021/src/day_2.rs
+++ aoc-2021/src/day_2.rs
@@ -19,4 +19,24 @@ pub mod day_2 {
 
         coords.0 * coords.1
     }
+
+    pub fn part_2() -> usize {
+        // (horizontal, vertical)
+        let coords = utils::read_lines("input/day-2.txt")
+            .iter()
+            // horizontal, depth, aim
+            .fold((0, 0, 0), |c, l| {
+                let mut tokens = l.split(' ');
+                let dir = tokens.next().unwrap();
+                let value = tokens.next().unwrap().parse::<usize>().unwrap();
+                match (dir, value) {
+                    ("forward", n) => (c.0 + n, c.1 + c.2 * n, c.2),
+                    ("up", n) => (c.0, c.1, c.2 - n),
+                    ("down", n) => (c.0, c.1, c.2 + n),
+                    _ => panic!("where we headed?"),
+                }
+            });
+
+        coords.0 * coords.1
+    }
 }
--- aoc-2021/src/main.rs
+++ aoc-2021/src/main.rs
@@ -3,5 +3,5 @@ mod day_2;
 mod utils;
 
 fn main() {
-    print!("answer: {}", day_2::day_2::part_1());
+    print!("answer: {}", day_2::day_2::part_2());
 }
