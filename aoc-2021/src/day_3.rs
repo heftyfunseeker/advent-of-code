a69de06b55f5f2db317413efccb267b4a50dbac3
nicco simone <niccosimone@niccos-MBP-2.domain>
2021-12-03 13:02:42 -0800

    meh let's use u32s instead of chars/strs

--- aoc-2021/src/day_3.rs
+++ aoc-2021/src/day_3.rs
@@ -1,29 +1,47 @@
 pub mod day_3 {
     use crate::utils;
 
-    pub fn part_1() -> u32 {
-        const NUM_BITS: usize = 12;
-        let mut one_bit_counts: [usize; NUM_BITS] = [0; NUM_BITS];
+    const NUM_BITS: usize = 12;
+    fn get_bit_counts_and_words() -> ([u32; NUM_BITS], Vec<u32>){
+        let mut one_bit_counts: [u32; NUM_BITS] = [0; NUM_BITS];
 
-        // I feel like I should just be using a xor bitwise op or something?
-        let lines = utils::read_lines("input/day-3.txt");
-        lines.iter().for_each(|l| {
-            l.chars().enumerate().for_each(|(bit_index, bit)| {
-                one_bit_counts[bit_index] += bit.to_digit(10).unwrap() as usize
-            })
+        let words: Vec<u32> = utils::read_lines("input/day-3.txt")
+            .iter()
+            .map(|l| u32::from_str_radix(l, 2).unwrap())
+            .collect();
+
+        words.iter().for_each(|word| {
+            for i in 0..NUM_BITS {
+                one_bit_counts[i] += u32::from((word & (1 << (NUM_BITS - 1 - i))) != 0)
+            }
         });
+        return (one_bit_counts, words);
+    }
 
-        let count_threshold = lines.len() / 2;
+    fn get_gamma(bit_counts: [u32; NUM_BITS], total_words: u32) -> u32 {
+        let count_threshold = total_words / 2;
         let mut gamma: u32 = 0;
-        one_bit_counts
+        bit_counts
             .into_iter()
             .enumerate()
             .for_each(|(bit_index, bit_count)| {
                 let bit = if bit_count > count_threshold { 1 } else { 0 };
                 gamma |= bit << (NUM_BITS - 1) - bit_index;
             });
+        return gamma;
+    }
 
-        let epsilon = !gamma & 0xFFF;
+    fn get_epsilon(gamma: u32) -> u32 {
+        !gamma & 0xFFF
+    }
+
+    pub fn part_1() -> u32 {
+        let (bit_counts, words) = get_bit_counts_and_words();
+        let gamma = get_gamma(bit_counts, words.len() as u32);
+        let epsilon = get_epsilon(gamma);
         gamma * epsilon
+    }
+
+    pub fn part_2() {
     }
 }
