dbad460b065a7808c91159b912cb5d9d68cd1745
nicco simone <niccosimone@niccos-MBP-2.domain>
2021-12-03 14:32:16 -0800

    day 3 part 2

--- aoc-2021/src/day_3.rs
+++ aoc-2021/src/day_3.rs
@@ -2,20 +2,21 @@ pub mod day_3 {
     use crate::utils;
 
     const NUM_BITS: usize = 12;
-    fn get_bit_counts_and_words() -> ([u32; NUM_BITS], Vec<u32>){
-        let mut one_bit_counts: [u32; NUM_BITS] = [0; NUM_BITS];
-
-        let words: Vec<u32> = utils::read_lines("input/day-3.txt")
+    fn get_words() -> Vec<u32> {
+        utils::read_lines("input/day-3.txt")
             .iter()
             .map(|l| u32::from_str_radix(l, 2).unwrap())
-            .collect();
+            .collect()
+    }
 
+    fn get_bit_counts(words: &Vec<u32>) -> [u32; NUM_BITS] {
+        let mut bit_counts: [u32; NUM_BITS] = [0; NUM_BITS];
         words.iter().for_each(|word| {
             for i in 0..NUM_BITS {
-                one_bit_counts[i] += u32::from((word & (1 << (NUM_BITS - 1 - i))) != 0)
+                bit_counts[i] += u32::from((word & (1 << (NUM_BITS - 1 - i))) != 0)
             }
         });
-        return (one_bit_counts, words);
+        return bit_counts;
     }
 
     fn get_gamma(bit_counts: [u32; NUM_BITS], total_words: u32) -> u32 {
@@ -25,8 +26,7 @@ pub mod day_3 {
             .into_iter()
             .enumerate()
             .for_each(|(bit_index, bit_count)| {
-                let bit = if bit_count > count_threshold { 1 } else { 0 };
-                gamma |= bit << (NUM_BITS - 1) - bit_index;
+                gamma |= u32::from(bit_count >= count_threshold) << (NUM_BITS - 1) - bit_index;
             });
         return gamma;
     }
@@ -36,12 +36,48 @@ pub mod day_3 {
     }
 
     pub fn part_1() -> u32 {
-        let (bit_counts, words) = get_bit_counts_and_words();
+        let words = get_words();
+        let bit_counts = get_bit_counts(&words);
         let gamma = get_gamma(bit_counts, words.len() as u32);
         let epsilon = get_epsilon(gamma);
         gamma * epsilon
     }
 
-    pub fn part_2() {
+    pub fn apply_filter_mask(words: Vec<u32>, mask: u32, bit_index: usize) -> Vec<u32> {
+        words
+            .iter()
+            .cloned()
+            .filter(|w| {
+                let mask_bit = mask & 1 << bit_index;
+                let word_bit = w & 1 << bit_index;
+                word_bit == mask_bit
+            })
+            .collect()
+    }
+
+    pub fn part_2() -> u32 {
+        let mut o2_gen_rating = get_words();
+        let mut co2_scrubber_rating = o2_gen_rating.clone();
+
+        for bit_index in (0..NUM_BITS).rev() {
+            if o2_gen_rating.len() == 1 {
+                break;
+            }
+            let bit_counts = get_bit_counts(&o2_gen_rating);
+            let gamma = get_gamma(bit_counts, o2_gen_rating.len() as u32);
+            o2_gen_rating = apply_filter_mask(o2_gen_rating, gamma, bit_index);
+        }
+
+        for bit_index in (0..NUM_BITS).rev() {
+            if co2_scrubber_rating.len() == 1 {
+                break;
+            }
+            let bit_counts = get_bit_counts(&co2_scrubber_rating);
+            let gamma = get_gamma(bit_counts, co2_scrubber_rating.len() as u32);
+            let epsilon = get_epsilon(gamma);
+            co2_scrubber_rating = apply_filter_mask(co2_scrubber_rating, epsilon, bit_index);
+        }
+
+        o2_gen_rating.pop().unwrap() * co2_scrubber_rating.pop().unwrap()
     }
 }
--- aoc-2021/src/main.rs
+++ aoc-2021/src/main.rs
@@ -4,5 +4,5 @@ mod day_3;
 mod utils;
 
 fn main() {
-    print!("answer: {}", day_3::day_3::part_1());
+    print!("answer: {}", day_3::day_3::part_2());
 }
